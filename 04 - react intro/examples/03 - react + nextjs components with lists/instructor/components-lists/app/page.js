0bd95e4b9bf75a96d44b55e846801f0a89d7d90c
oaao <oliver+github@abolish.golf>
2026-02-24 08:53:13 -0700

    add [04 - react intro]: ex03 - pass list as prop to component

--- 04 - react intro/examples/03 - react + nextjs components with lists/instructor/components-lists/app/page.js
+++ 04 - react intro/examples/03 - react + nextjs components with lists/instructor/components-lists/app/page.js
@@ -1,10 +1,13 @@
 import styles from "./page.module.css";
 
+// components
+import SimpsonsCharacters from './components/SimpsonsCharacters'
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-
+        <SimpsonsCharacters />
       </main>
     </div>
   );
